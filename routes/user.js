134d954e105d7fbede9e21ac3a7f45f79db1c8ea
Judyy22 <115631038+Judyy22@users.noreply.github.com>
2023-06-09 14:51:41 +0900

    swagger get/post 예제 완료

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,89 @@
 const router = require("express").Router();
 const users = [
-    { id: "hong", pw: "gil", nickname: "gildong" },
-    { id: "baby", pw: "dinosaur", nickname: "dooly" },
-    { id: "hello", pw: "hihi", nickname: "Kitty" },
+    { id: "hong", name: "gildong" },
+    { id: "baby", name: "dooly" },
+    { id: "hello", name: "Kitty" },
 ];
 
 /**
  * @swagger
  * paths:
- *  /user/nickname:
- *   post:
- *    tags:
- *    - user
- *    description: 닉네임 조회
+ *  /user/userNames:
+ *    get:
+ *      description: "서버에 데이터를 보내지 않고 Get방식으로 요청"
+ *      tags: [userNames]
+ *      responses:
+ *        200:
+ *          description: 전체 유저 정보
+ *          schema:
+ *            properties:
+ *                    users:
+ *                      type: object
+ *          401:
+ *              description: 닉네임 조회 실패
+ *              schema:
+ *               properties:
+ *                   message:
+ *                       type: string
+ */
+router.get("/userNames", async (req, res, next) => {
+    try {
+        res.json({ users: users });
+    } catch (error) {
+        console.log(error);
+        return next(error);
+    }
+});
+
+/**
+ * @swagger
+ * /user/userName?user_id={user_id}:
+ *  get:
+ *    summary: "특정 유저조회 Query 방식"
+ *    description: "요청 경로에 값을 담아 서버에 보낸다."
+ *    tags: [Users]
+ *    parameters:
+ *      - in: query
+ *        name: user_id
+ *        required: true
+ *        description: 유저 아이디
+ *        schema:
+ *          type: string
+ *    responses:
+ *      "200":
+ *        description: 사용자가 서버로 전달하는 값에 따라 결과 값은 다릅니다. (유저 조회)
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                users:
+ *                  type: object
+ *                  example: [{ "id": 1, "name": "유저1" }]
+ */
+router.get("/userName", async (req, res, next) => {
+    try {
+        const user_id = req.query.user_id;
+
+        const user = users.filter((data) => data.id == user_id);
+
+        res.json({ user: user });
+    } catch (error) {
+        console.log(error);
+        return next(error);
+    }
+});
+
+/**
+ * @swagger
+ *
+ * /user/addUser:
+ *  post:
+ *    summary: "유저 등록"
+ *    description: "POST 방식으로 유저를 등록한다."
+ *    tags: [AddUser]
+ *    requestBody:
+ *      description: 사용자가 서버로 전달하는 값에 따라 결과 값은 다릅니다. (유저 등록)
  *    parameters:
  *    - in: body
  *      name: body
@@ -21,43 +92,30 @@ const users = [
  *       properties:
  *        id:
  *         type: string
- *        pw:
+ *        name:
  *         type: string
- *
  *    responses:
  *     200:
- *      description: 닉네임 조회 성공
+ *      description: 유저 등록 성공
  *      schema:
  *       properties:
  *        message:
  *         type: string
  *     401:
- *      description: 닉네임 조회 실패
+ *      description: 유저 등록 실패
  *      schema:
  *       properties:
  *        message:
  *         type: string
  *
  */
-
-router.post("/nickname", async (req, res, next) => {
+router.post("/addUser", async (req, res, next) => {
     try {
-        for (var i = 0; i < users.length; i++) {
-            if (req.body.id == users[i].id) {
-                if (req.body.pw == users[i].pw) {
-                    return res.status(200).json({
-                        message: "닉네임 : " + users[i].nickname,
-                    });
-                } else {
-                    return res.status(401).json({
-                        message: "비밀번호가 틀렸습니다!",
-                    });
-                }
-            }
-        }
-        return res.status(401).json({
-            messge: "아이디가 존재하지 않습니다!",
-        });
+        const { id, name } = req.body;
+
+        const user = users.concat({ id, name });
+
+        res.json({ users: user });
     } catch (error) {
         console.log(error);
         return next(error);
